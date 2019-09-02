ed26b71c693f47ee76b7c0e0c6a4d6ef6f02525c
duskamo <dyanownz@yahoo.com>
2019-09-02 15:19:06 -0500

    Email sends out to owners when house gets booked through web app.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import os
 from src.EmailClient import EmailClient
+from src.Data import Data
 app = Flask(__name__)
 
 # Data POST Requests
@@ -21,6 +22,25 @@ def send_contact():
 
 	# Return status code
 	return "200"
+
+@app.route('/email_booking_confirmation_to_owner', methods=['POST'])
+def email_booking_confirmation_to_owner():
+	# Gather Data
+	bookingInfo = request.json
+	
+	# Send email to dlandry email server using client contact info
+	emailClient = EmailClient()
+
+	emailClient.addUserEmail(bookingInfo['contactInfo']['email'])
+	emailClient.addUserName(bookingInfo['contactInfo']['firstName'] + " " + bookingInfo['contactInfo']['lastName'])
+	emailClient.addSubject(Data.bookedSubject)
+	emailClient.addMessage(Data.bookedMessage.format(bookingInfo['rentalInfo']['arrivalDate'],bookingInfo['rentalInfo']['departDate']))
+
+	emailClient.send()
+
+	# Return status code
+	return "200"
+
 
 # Run app on 0.0.0.0:5001
 if __name__ == "__main__":
--- src/Data.py
+++ src/Data.py
@@ -3,3 +3,7 @@ class Data:
 	clientEmail = "dyanownz@gmail.com"
 	clientPassword = "Blueballs69"
 	serverEmail = "contact@dlandrybeachhouses.com"
+
+	# Email Confirmation to owners when dates are booked
+	bookedSubject = "Booked dates through website"
+	bookedMessage = "Your website has a new booking for the dates {0} through {1}."
